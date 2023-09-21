725569bda33baaf0552f9b34380d4a4cd3841c9c
Deev saini <85650705+deevsaini@users.noreply.github.com>
2023-09-21 12:58:20 +0530

    Update constants.dart

--- lib/config/utils/constants.dart
+++ lib/config/utils/constants.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static const Color primaryColor = Color(0xFFE1A514);
-  static const Color primaryColorAccent = Color(0xFFFBC22E);
+  static const Color primaryColor = Colors.deepPurple;
+  static const Color primaryColorAccent = Colors.deepPurpleAccent;
   static const Color royalBlue = Color(0xFF064BB5);
   static const Color blue = Colors.blue;
   static Color blueAccent = Colors.blue[100]!;
@@ -11,13 +11,5 @@ class Constants {
   static const Color bg = Color(0xFFF5F5F6);
 
   static const String userDb = "user_box";
-  // static const String baseUrl = "http://192.168.1.27:3000/api/";
-  static const String baseUrl = "https://couponman-admin.vercel.app/api/";
-  static const String loginUrl = "auth/login";
-  static const String registerUrl = "auth/register";
-  static const String getUserProfileUrl = "user/get-profile";
-  static const String updateUserUrl = "user/update";
-  static const String createPostUrl = "post/create";
-  static const String getUserPosts = "user/get-posts";
-  static const String getAllPosts = "post/get-all";
+  static const String baseUrl = "";
 }
